92ea7ee79e83ed0602bc6950ec8da89c517db5ab
abhaybabar009 <abhaybabar781gmail.com>
2026-03-26 11:20:00 +0530

    project 1

---
+++ script.js
@@ -0,0 +1,46 @@
+const form = document.getElementById("moodForm");
+const moodInput = document.getElementById("mood");
+const songsDiv = document.getElementById("songs");
+const error = document.getElementById("error");
+
+// Data (songs)
+const musicData = {
+    happy: ["Happy - Pharrell", "Good Life - OneRepublic"],
+    sad: ["Let Her Go - Passenger", "Fix You - Coldplay"],
+    energetic: ["Believer - Imagine Dragons", "Stronger - Kanye West"]
+};
+
+// Form submit
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    songsDiv.innerHTML = "";
+    error.textContent = "";
+
+    const mood = moodInput.value;
+
+    // Validation
+    if (mood === "") {
+        error.textContent = "Please select a mood!";
+        return;
+    }
+
+    // Show songs using Promise
+    getSongs(mood)
+        .then(data => {
+            data.forEach(song => {
+                const p = document.createElement("p");
+                p.textContent = song;
+                songsDiv.appendChild(p);
+            });
+        });
+});
+
+// Promise function
+function getSongs(mood) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(musicData[mood]);
+        }, 1000);
+    });
+}
